f062c12659a4aa724ad03bbec0a2d6a16ef1e7c3
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2026-01-07 15:21:33 -0300

    refactor: migrate callbacks to async/await across the project

--- index.js
+++ index.js
@@ -2,41 +2,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const chalk = require('chalk');
 
-
 async function main() {
-    try {
-        const answers = await inquirer.prompt([
-          {
-          type: 'list',
-          name: 'action',
-          message: chalk.bgBlueBright('Welcome to the Movie Manager CLI!'),
-          choices: ['Type to add a movie', 'Type to delete a movie', 'Type to view all movies', 'Type to view movies by genre', 'Exit']
+    let running = true;
+    
+    while(running) {
+        try {
+            console.log('\n');
+            const answers = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: chalk.bgBlueBright('Welcome to the Movie Manager CLI!'),
+                    choices: ['Type to add a movie', 'Type to delete a movie', 'Type to view all movies', 'Type to view movies by genre', 'Exit']
+                }
+            ]);
+
+            switch (answers.action) {
+                case 'Type to add a movie':
+                    await addMovie();       
+                    break;
+                case 'Type to delete a movie':
+                    await deleteMovie();    
+                    break;
+                case 'Type to view all movies':
+                    await viewMovies();
+                    break;
+                case 'Type to view movies by genre':
+                    await viewMoviesByGenre();  
+                    break;
+                case 'Exit':
+                    console.log(chalk.green('Goodbye!'));
+                    running = false;
+                    process.exit(0);
+            }
+        } catch (error) {
+            console.error(chalk.red('An error occurred:'), error);
         }
-    ]);
-        switch (answers.action) {
-            case 'Type to add a movie':
-                await addMovie();       
-                break;
-            case 'Type to delete a movie':
-                await deleteMovie();    
-                break;
-            case 'Type to view all movies':
-                await viewMovies();
-                break;
-            case 'Type to view movies by genre':
-                await viewMoviesByGenre();  
-                break;
-            case 'Exit':
-                console.log(chalk.green('Goodbye!'));
-                process.exit(0);
-        }
-    } catch (error) {
-        console.error(chalk.red('An error occurred:'), error);
     }
 }
 
-function addMovie() {
-  inquirer.prompt([
+async function addMovie() {
+  const movies = await loadMovies(); 
+  const existingGenres = movies.map(movie => movie.genre);
+  
+  const uniqueGenres = [...new Set(existingGenres)];
+  
+  const genreChoices = [...uniqueGenres, ' Add new genre'];
+
+  const answers = await inquirer.prompt([
     {
       type: 'input',
       name: 'title',
@@ -47,48 +60,62 @@ function addMovie() {
       name: 'year',
       message: 'Enter the movie year:'
     },
-    {      type: 'input',
+    {      
+      type: 'input',
       name: 'description',
       message: 'Enter the movie description:'
     },
     {
-      type: 'input',
+      type: 'list',
       name: 'genre',
-      message: 'Enter the movie genre:'
+      message: 'Enter the movie genre:',
+      choices: genreChoices
     }
-  ]).then(answers => {
-    const movies = loadMovies();
-    movies.push({ title: answers.title, genre: answers.genre, year: answers.year, description: answers.description });
-    saveMovies(movies);
-    console.log(chalk.green('Movie added successfully!'));
-    main();
+  ]);
+
+  let finalGenre = answers.genre;
+  
+  if (answers.genre === ' Add new genre') {
+      const newGenreAnswer = await inquirer.prompt([
+          { type: 'input', name: 'newGenre', message: 'Enter the new genre name:' }
+      ]);
+      finalGenre = newGenreAnswer.newGenre;
+  }
+
+  movies.push({ 
+      title: answers.title, 
+      genre: finalGenre, 
+      year: answers.year, 
+      description: answers.description 
   });
+
+  await saveMovies(movies);
+  console.log(chalk.green('Movie added successfully!'));
 }
 
-
-function deleteMovie() {  
-  const movies = loadMovies();
+async function deleteMovie() {  
+  const movies = await loadMovies();
   if (movies.length === 0) {
     console.log(chalk.yellow('No movies to delete.'));
-    return main();
+    return; 
   }
-  inquirer.prompt([
+  
+  const answer = await inquirer.prompt([
     {
       type: 'list',
       name: 'title',
       message: 'Select a movie to delete:',
       choices: movies.map(movie => movie.title)
     }
-  ]).then(answer => {
-    const updatedMovies = movies.filter(movie => movie.title !== answer.title);
-    saveMovies(updatedMovies);
-    console.log(chalk.green('Movie deleted successfully!'));
-    main();
-  }
-  );
+  ]);
+
+  const updatedMovies = movies.filter(movie => movie.title !== answer.title);
+  await saveMovies(updatedMovies);
+  console.log(chalk.green('Movie deleted successfully!'));
 }
-function viewMovies() {
-  const movies = loadMovies();
+
+async function viewMovies() {
+  const movies = await loadMovies();
   if (movies.length === 0) {
     console.log(chalk.yellow('No movies found.'));
   }
@@ -98,12 +125,15 @@ function viewMovies() {
       console.log(`${index + 1}. ${movie.title} - Genre: ${movie.genre} - Year: ${movie.year}`);
     });
   }
-  main();
-}async function viewMoviesByGenre() {
-  const movies = loadMovies();
+  // Pausa para leitura
+  await inquirer.prompt([{type: 'input', name: 'wait', message: 'Press enter to return'}]);
+} 
+
+async function viewMoviesByGenre() {
+  const movies = await loadMovies();
   if (movies.length === 0) {
     console.log(chalk.yellow('No movies found.'));
-    return await main();
+    return;
   }
 
   const genres = [...new Set(movies.map(movie => movie.genre))];
@@ -115,7 +145,9 @@ function viewMovies() {
       choices: genres
     }
   ]);
+  
   const filteredMovies = movies.filter(movie => movie.genre === answer.genre);
+  
   if (filteredMovies.length === 0) {
     console.log(chalk.yellow('No movies found in this genre.'));
   }
@@ -126,12 +158,12 @@ function viewMovies() {
       console.log(`   Description: ${movie.description}`);
     });
   }
-  await main();
+  await inquirer.prompt([{type: 'input', name: 'wait', message: 'Press enter to return'}]);
 }
 
-function loadMovies() {
+async function loadMovies() {
   try {
-    const dataBuffer = fs.readFileSync('movies.json');    
+    const dataBuffer = await fs.promises.readFile('movies.json');    
     const dataJSON = dataBuffer.toString();    
     return JSON.parse(dataJSON);  
   } catch (e) {
@@ -139,9 +171,10 @@ function loadMovies() {
   }
 }
 
-function saveMovies(movies) {
-  const dataJSON = JSON.stringify(movies);  
-  fs.writeFileSync('movies.json', dataJSON);
+async function saveMovies(movies) {
+  const dataJSON = JSON.stringify(movies, null, 2); 
+  await fs.promises.writeFile('movies.json', dataJSON);
 }
+
 
 main();
